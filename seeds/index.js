c1d756827244d371c326616442cdc573e0eb7885
Anish Ahuja <anishahuja@Anishs-MacBook-Air.local>
2022-01-31 23:14:44 -0500

    version 2.1

--- seeds/index.js
+++ seeds/index.js
@@ -3,7 +3,12 @@ const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp');
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+  }
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+
+mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
 
@@ -17,14 +22,16 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for (let i = 0; i < 300; i++) {
+    for (let i = 0; i < 1000; i++) {
         const random1000 = Math.floor(Math.random() * 2739);
         const price = Math.floor(Math.random() * 20) + 10;
+        const descrip = sample(descriptors);
+        const place = sample(places);
         const camp = new Campground({
-            author: '61ecbf9950214883662b8a81',
+            author: '61f890ef89ea6d2452c9f031',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`,
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
+            title: `${descrip} ${place}`,
+            description: `Seek adventure at ${descrip} ${place}, where experiences engage you — from mountain biking through the woods, to camping along the riverside, to running in adventure races. Spend an afternoon, or a whole weekend!`,
             price,
             geometry: {
                 type: "Point",
